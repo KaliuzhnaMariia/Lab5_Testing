0099696394376d8a3d0c77c6f81cb6218ef22c4c
Masha <124194241+KaliuzhnaMariia@users.noreply.github.com>
2025-04-19 02:45:54 +0300

    Delete song-manager directory

--- song-manager/src/main/java/com/example/songs/repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.songs.repository;
-
-import com.example.songs.model.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-}
