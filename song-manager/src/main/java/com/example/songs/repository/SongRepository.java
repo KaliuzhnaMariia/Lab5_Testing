3004f9924f450580774015d7c249ff83d4e0e863
Masha <124194241+KaliuzhnaMariia@users.noreply.github.com>
2025-04-24 23:51:20 +0300

    Update SongRepository for testing

--- song-manager/src/main/java/com/example/songs/repository/SongRepository.java
+++ song-manager/src/main/java/com/example/songs/repository/SongRepository.java
@@ -3,5 +3,8 @@ package com.example.songs.repository;
 import com.example.songs.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+    Optional<Song> findFirstByTitle(String title);
 }
